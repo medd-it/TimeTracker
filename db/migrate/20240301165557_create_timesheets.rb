d83f6c8cd4a7e6e73bd1ee695aefb98eb4463204
medd <m.daldegan@e-time.it>
2024-03-01 17:59:36 +0100

    fixed timesheet model references

--- db/migrate/20240301165557_create_timesheets.rb
+++ db/migrate/20240301165557_create_timesheets.rb
@@ -1,8 +1,8 @@
 class CreateTimesheets < ActiveRecord::Migration[7.1]
   def change
     create_table :timesheets do |t|
-      t.references :user_id, null: false, foreign_key: true
-      t.references :project_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :project, null: false, foreign_key: true
       t.string :notes
       t.datetime :start
       t.datetime :end

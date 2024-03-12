4e024e9922d37109d3e7e894bb2e19a98ffbf8ac
medd <it.medd@gmail.com>
2024-03-12 15:45:54 +0100

    fixed bug in update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if User.update(user_params)
+    if @user.update(user_params)
       redirect_to user_path(@user.id)
     else
       render('edit')

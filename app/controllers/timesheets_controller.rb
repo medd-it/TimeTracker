da266569eeadaf5fe17caad6c474a56ac8476004
medd <it.medd@gmail.com>
2024-03-12 16:54:39 +0100

    timesheets new and create actions + view

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -7,9 +7,16 @@ class TimesheetsController < ApplicationController
   end
 
   def new
+    @timesheet = Timesheet.new
   end
 
   def create
+    @timesheet = Timesheet.new(timesheet_params)
+    if @timesheet.save
+      redirect_to timesheets_path
+    else
+      render('new')
+    end
   end
 
   def edit
@@ -22,5 +29,11 @@ class TimesheetsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def timesheet_params
+    params.require(:timesheet).permit(:user_id, :project_id, :start, :end, :notes)
   end
 end
